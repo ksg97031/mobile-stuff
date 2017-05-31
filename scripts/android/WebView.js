ed171f753b9a618afdf4f3d0277ef23160cf3af9
CodeColorist <ChiChou@users.noreply.github.com>
2017-05-31 22:20:21 +0800

    Update WebView.js

--- scripts/android/WebView.js
+++ scripts/android/WebView.js
@@ -1,13 +1,28 @@
-Java.perform(function () {
-    var WebView = Java.use("android.webkit.WebView");
-    var CookieManager = Java.use("android.webkit.CookieManager");
-    var cookieManager = CookieManager.getInstance();
+Java.perform(function() {
+  var WebView = Java.use('android.webkit.WebView');
+  var CookieManager = Java.use("android.webkit.CookieManager");
+  var cookieManager = CookieManager.getInstance();
 
-    WebView.loadUrl.overload("java.lang.String").implementation = function (s) {
-        var cookie = cookieManager.getCookie.overload("java.lang.String").call(cookieManager, s) || '(empty)';
-        console.log('url:', JSON.stringify(s.toString()));
-        console.log('cookie:', JSON.stringify(cookie.toString()));
-        this.loadUrl.overload("java.lang.String").call(this, s);
-    };
+  var Context = Java.use('android.content.Context');
+  var WebViewClient = Java.use('android.webkit.WebViewClient');
+  var client = WebViewClient.$new();
 
-});
+  WebViewClient.shouldOverrideUrlLoading.overload('android.webkit.WebView', 'java.lang.String').implementation = function(view, url) {
+    var cookie = cookieManager.getCookie.overload("java.lang.String").call(cookieManager, s) || '(empty)';
+    console.log('url:', JSON.stringify(s.toString()));
+    console.log('cookie:', JSON.stringify(cookie.toString()));
+
+    return true
+  };
+
+
+  WebView.loadUrl.overload("java.lang.String").implementation = function (s) {
+    var cookie = cookieManager.getCookie.overload("java.lang.String").call(cookieManager, s) || '(empty)';
+    console.log('url:', JSON.stringify(s.toString()));
+    console.log('cookie:', JSON.stringify(cookie.toString()));
+
+    this.setWebViewClient(client);
+    return this.loadUrl.overload("java.lang.String").call(this, s);
+  };
+
+})
